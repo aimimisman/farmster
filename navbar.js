8835adefd9a3c3f17b814d9a5506ef862f6e0a5d
ellyanaAfiera <110796485+ellyanaAfiera@users.noreply.github.com>
2026-06-20 09:10:00 +0800

    Update URL in all page

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  fetch("/nav.html")
+  fetch("https://aimimisman.github.io/farmster/nav.html")
     .then(res => res.text())
     .then(html => {
 
@@ -28,5 +28,5 @@ function logoutUser() {
     localStorage.removeItem("currentUser");
 
     window.location.href =
-   "/modules/member-b/farmer-profile/login.html";
+   "https://aimimisman.github.io/farmster/modules/member-b/farmer-profile/login.html";
 }
